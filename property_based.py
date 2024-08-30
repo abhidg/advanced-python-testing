88007e9cbbe52b33ae8fa010402842f6a042f242
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-08-30 18:16:04 +0100

    Add slides draft

---
+++ property_based.py
@@ -0,0 +1,6 @@
+from hypothesis import given
+from hypothesis.strategies import floats, integers
+def divide(x, y): return x / y
+
+@given(integers(), integers())
+def test_divide(x, y): assert divide(x, y) < x
